8402836fb489483d0e762dccdc781f26d3a8f942
Shawn Bradley <shawn.bradley@apleto.com>
2023-09-21 17:35:08 -0500

    mongodb

--- src/mongodb.js
+++ src/mongodb.js
@@ -4,7 +4,10 @@ module.exports = function (app) {
   const connection = app.get('mongodb');
   const database = connection.substr(connection.lastIndexOf('/') + 1);
   const mongoClient = MongoClient.connect(connection)
-    .then(client => client.db(database));
+    .then(client => client.db(database))
+    .catch(e => {
+      console.log(e)
+    })
 
   app.set('mongoClient', mongoClient);
 };
