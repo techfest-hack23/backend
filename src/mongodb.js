d50567ce92af276df48b85bc2e68a727c159103f
kameron90d <kameron90d@gmail.com>
2023-09-21 19:45:08 -0500

    Update mongodb.js

--- src/mongodb.js
+++ src/mongodb.js
@@ -2,7 +2,8 @@ const MongoClient = require('mongodb').MongoClient;
 
 module.exports = function (app) {
   const connection = app.get('mongodb');
-  const database = connection.substr(connection.lastIndexOf('/') + 1);
+  //const database = connection.substr(connection.lastIndexOf('/') + 1);
+  const database = 'techfest';
   const mongoClient = MongoClient.connect(connection)
     .then(client => client.db(database))
     .catch(e => {
