cc356813ded3ea1c67068cb536ff08c693619272
kameron90d <kameron90d@gmail.com>
2023-09-21 19:22:39 -0500

    Update mongodb.js

--- src/mongodb.js
+++ src/mongodb.js
@@ -3,7 +3,7 @@ const MongoClient = require('mongodb').MongoClient;
 module.exports = function (app) {
   const connection = app.get('mongodb');
   const database = connection.substr(connection.lastIndexOf('/') + 1);
-  const mongoClient = MongoClient.connect(connection)
+  const mongoClient = MongoClient.connect(username='techfest23', password='Welcome1', host=docdb-2023-09-21-21-56-02.cluster-cnnexkzeq0db.us-east-1.docdb.amazonaws.com, port=27017)
     .then(client => client.db(database));
 
   app.set('mongoClient', mongoClient);
